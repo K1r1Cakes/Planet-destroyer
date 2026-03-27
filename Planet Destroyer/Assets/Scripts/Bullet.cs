012fb09ed23c84a2802b20ba6bf1f667e07d6744
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-27 22:09:35 +0200

    Main | Bullets shooting

---
+++ Planet Destroyer/Assets/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   public float bulletSpeed = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime);
+        if (transform.position.x > 8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Planet Destroyer/Assets/Scripts/Player Shoot.cs
+++ Planet Destroyer/Assets/Scripts/Player Shoot.cs
@@ -22,6 +22,6 @@ public class PlayerShoot : MonoBehaviour
     {
         if (!context.started) return;
 
-        Debug.Log("Shoot");
+        Instantiate(bulletPrefab, transform.position, Quaternion.identity);
     }
 }
