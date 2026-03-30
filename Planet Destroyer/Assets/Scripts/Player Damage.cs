59ab5d900b36b73b1658f0c29cf64ac9c70b9a4c
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-30 17:57:35 +0200

    Main | Player takes planet damage

--- Planet Destroyer/Assets/Scripts/Player Damage.cs
+++ Planet Destroyer/Assets/Scripts/Player Damage.cs
@@ -20,6 +20,25 @@ public class PlayerDamage : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("planet"))
+        {
+            Debug.Log("Planet hit");
+            health.TakeDamage(damage);
+            sprite.color = Color.red;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+         Color normalColour;
+        if (ColorUtility.TryParseHtmlString("#58DEFF", out normalColour))
+        {
+            sprite.color = normalColour;
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collide)
     {
         Color normalColour;
