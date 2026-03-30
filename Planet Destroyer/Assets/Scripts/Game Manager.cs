fb5c954a7b0db51c75504f82cb019b80a1b5ee3b
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-30 17:41:12 +0200

    Main | Planet and Obstacle movement

---
+++ Planet Destroyer/Assets/Scripts/Game Manager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] spawnPoints;
+    public GameObject[] spawnPointsP;
+    public float Obstacletimer;
+    public float planetTimer;
+    public float timeBetweenSpawns;
+    public float timeBetweenSpawnsPlanet;
+    public GameObject obstacle;
+    public GameObject planet;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Obstacletimer += Time.deltaTime;
+        planetTimer += Time.deltaTime;
+
+        if (Obstacletimer > timeBetweenSpawns)
+        {
+            Obstacletimer = 0;
+            int randNum = Random.Range(0, 3);
+            Instantiate(obstacle, spawnPoints[randNum].transform.position, Quaternion.identity);
+        }
+
+          if (planetTimer > timeBetweenSpawnsPlanet)
+        {
+            planetTimer = 0;
+            int randNum = Random.Range(0, 2);
+            Instantiate(planet, spawnPointsP[randNum].transform.position, Quaternion.identity);
+        }
+    }
+}
